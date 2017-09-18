9034447394522be25dfd4645e4831678f99fd5a8
joshuachung727 <joshua.chung@ocsbstudent.ca>
2017-09-18 11:13:09 -0400

    Play Area Set Up

--- Assets/Script/PlayerMoveControl.cs
+++ Assets/Script/PlayerMoveControl.cs
@@ -17,6 +17,11 @@ public class PlayerMoveControl : MonoBehaviour {
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown("space"))
+            rb.AddForce(Vector3.up*speed/10,ForceMode.Impulse);
+
+        if (Input.GetKeyDown("m"))
+            rb.AddForce(-Vector3.up * speed / 5, ForceMode.Impulse);
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
