e30065906016748e2548f250e0ba5440820fc52f
joshua chung <joshua.chung@ocsbstudent.ca>
2017-09-19 09:47:04 -0400

    added count text

--- Assets/Script/PlayerMoveControl.cs
+++ Assets/Script/PlayerMoveControl.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerMoveControl : MonoBehaviour {
 
     public float speed;
+    public Text countText;
 	public bool big=true;
 	public float ballScale = 1;
     private Rigidbody rb;
+    private int count;
 	Vector3 vec;
 	
     void Start()
@@ -15,6 +18,8 @@ public class PlayerMoveControl : MonoBehaviour {
 		vec = new Vector3 (1,1,1);
 		vec *= ballScale;
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        countText.text = "Count: " + count;
     }
 
     void FixedUpdate()
@@ -33,5 +38,17 @@ public class PlayerMoveControl : MonoBehaviour {
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag ( "pickUps"))
+        {
+            count++;
+            countText.text = "Count: " + count;
+            other.gameObject.SetActive(false);
+
+        }
     }
 }
---
+++ Assets/Script/tiltPlatForm.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tiltPlatForm : MonoBehaviour {
+    Vector3 vec;
+	// Use this for initialization
+	void Start () {
+        vec = new Vector3(1,5,4);
+	}
+	
+	
+	void Update () {
+        transform.Rotate(vec*Time.deltaTime);
+	}
+}
